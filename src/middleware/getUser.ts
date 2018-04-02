d668788868cacfbb4248a2e264aa6497c1db4f2f
Sammy <picosam@gmail.com>
2018-04-02 18:08:25 +0200

    refactor: remove the dots at the end of error messages

--- src/middleware/getUser.ts
+++ src/middleware/getUser.ts
@@ -10,7 +10,7 @@ export const getUser = async (req, res, next, db: Prisma) => {
     where: { auth0Id: req.token.sub }
   });
   if (!req.user) {
-    console.error(`User ${req.token.sub} does not exist.`);
+    console.error(`User ${req.token.sub} does not exist`);
     req.user = '404';
   }
   next();
